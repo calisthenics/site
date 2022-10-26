29b0d5f314d742607963347854ecf7e8742a3841
Ramiro Gómez <code@ramiro.org>
2022-10-26 22:44:59 +0200

    Update deploy script Remove Google Analytics and Disqus

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+
+deploy_repo=~/repos/deploy/calisthenics.github.io
+
 logya gen
-git clone git@github.com:calisthenics/calisthenics.github.io.git
-mv calisthenics.github.io/.git deploy/
-cd deploy/
+
+rsync -aruvz --exclude=.git --exclude=.gitignore --delete deploy/ "$deploy_repo"
+
+cd "$deploy_repo"
 git add .
-git commit -am 'new deployment'
+git commit -am'new deployment'
 git push
-cd ..
-rm -rf deploy/.git calisthenics.github.io/
